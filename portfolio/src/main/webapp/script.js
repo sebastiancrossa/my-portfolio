6e04aa25fca2af1f8c32be53ba8b80c5fd0e49c4
Sebastian Crossa <sebastiancrossa@hotmail.com>
2020-02-28 19:51:20 -0600

    Comments array now displays on the portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -28,11 +28,14 @@ function addRandomGreeting() {
 }
 
 /**
- * Fetches the data from our Java servlet 
+ * Fetches the data from our Java servlet and parses the received JSON
  */
 const getData = async () => {
     const res = await fetch('/data');
     const data = await res.text();
+
+    // Parsing our fetched JSON so it can be mapped through later on
+    const parsedData = JSON.parse(data); 
 
     document.getElementById('fetch-container').innerText = data;
 }
